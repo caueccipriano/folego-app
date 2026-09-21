9def2fcf767feee6d18f388f67f3219f238252de
caueccipriano <caueccipriano@gmail.com>
2026-09-20 22:32:14 -0300

    qa v1.7: regress notification detail routing

--- folego_flutter/test/push_route_navigation_test.dart
+++ folego_flutter/test/push_route_navigation_test.dart
@@ -1,8 +1,38 @@
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:folego/core/navigation/push_route.dart';
 import 'package:folego/features/shell/home_shell.dart';
 
 void main() {
+  test('notification routes preserve the requested financial detail', () {
+    expect(pushWalletDebtId('/wallet/debt/debt-1'), 'debt-1');
+    expect(pushWalletDebtId('/wallet'), isNull);
+
+    final invoice = pushWalletInvoiceTarget(
+      '/wallet/card/card-1/invoice/invoice-1',
+    );
+    expect(invoice, isNotNull);
+    expect(invoice!.cardId, 'card-1');
+    expect(invoice.invoiceId, 'invoice-1');
+
+    final subscription = pushRecurringRouteTarget(
+      '/transactions/subscriptions/sub-1',
+    );
+    expect(subscription, isNotNull);
+    expect(subscription!.itemId, 'sub-1');
+    expect(subscription.subscription, isTrue);
+
+    final recurring = pushRecurringRouteTarget(
+      '/transactions/recurring/rec-1',
+    );
+    expect(recurring, isNotNull);
+    expect(recurring!.itemId, 'rec-1');
+    expect(recurring.subscription, isFalse);
+
+    expect(pushRecurringRouteTarget('/transactions'), isNull);
+    expect(pushWalletInvoiceTarget('/wallet/card/card-1'), isNull);
+  });
+
   test('push routes open the related primary app tab', () {
     expect(homeIndexForPushRoute(null), 0);
     expect(homeIndexForPushRoute('/'), 0);
