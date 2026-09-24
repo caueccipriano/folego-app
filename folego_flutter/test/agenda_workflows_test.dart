04465259f036afa275db0b2fd253a76498efaae4
caueccipriano <caueccipriano@gmail.com>
2026-09-24 15:05:38 -0300

    test: align agenda navigation assertions with current copy

--- folego_flutter/test/agenda_workflows_test.dart
+++ folego_flutter/test/agenda_workflows_test.dart
@@ -264,7 +264,7 @@ void main() {
     await _flush(tester);
     await tester.tap(find.text('Dívida teste'));
     await _flush(tester);
-    expect(find.text('dívida'), findsOneWidget);
+    expect(find.text('detalhes da dívida'), findsOneWidget);
     expect(find.text('Dívida teste'), findsWidgets);
   });
 
@@ -303,9 +303,9 @@ void main() {
       ),
     );
     await _flush(tester);
-    expect(find.text('próximos dias'), findsOneWidget);
+    expect(find.text('próximos movimentos'), findsOneWidget);
     expect(find.textContaining('Internet'), findsOneWidget);
-    await tester.tap(find.text('próximos dias'));
+    await tester.tap(find.text('próximos movimentos'));
     await _flush(tester);
     expect(find.text('agenda'), findsOneWidget);
   });
